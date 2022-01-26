d6730baf59735819c99bc3c9eac08fe95f06435e
souvik-xavi <61917925+souvik-xavi@users.noreply.github.com>
2022-01-26 21:25:47 +0530

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import {HttpClientModule} from '@angular/common/http';
+//toast
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+//font awasome
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), 
+    CommonModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit,Input } from '@angular/core';
+import {
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faDatabase,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input()
+  user:any;
+  faEnvelope = faEnvelope;
+  faMapMarkedAlt = faMapMarkedAlt;
+  faPhone = faPhone;
+  faDatabase = faDatabase;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  reloadCurrentPage() {
+    window.location.reload();
+   }
+
+}
